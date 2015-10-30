0c325b18c3f836edff196ff0d360b1af0cfef4f2
Harlan Haskins <harlan@harlanhaskins.com>
2015-10-30 18:52:34 -0400

    Added arguments to CLI

--- app/Main.hs
+++ app/Main.hs
@@ -4,15 +4,30 @@ import Prelude hiding (exp)
 import Letter.Core
 import Letter.Parser
 import Text.Megaparsec.ByteString
+import Options.Applicative
 import System.Environment
+
+data Config = Config
+            { filename :: String
+            , dump     :: Bool
+            } deriving Show
+
+config = Config
+     <$> argument str
+         ( metavar "FILE" )
+     <*> switch
+         ( long "dump-ast"
+        <> short 'd'
+        <> help "Dump the AST instead of evaluating" )
 
 main :: IO ()
 main = do
-    [filename] <- getArgs
+    (Config filename dump) <- execParser (info config fullDesc)
     p <- parseFromFile parseFile filename
-    print p
-    case p of
-        (Left err) -> print err
-        (Right (env, exps)) -> do
-            val <- eval env (Do exps)
-            print val
+    if dump then print p
+    else do
+        case p of
+            (Left err) -> print err
+            (Right (env, exps)) -> do
+                val <- eval env (Do exps)
+                print val
